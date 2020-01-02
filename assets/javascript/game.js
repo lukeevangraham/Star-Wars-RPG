e0819989764244b0d02245fec519056f619bc6c6
Cefalu Wave <luke.evan.graham@gmail.com>
2020-01-01 21:45:53 -0800

    getting all characters visible

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 let char1 = {
     name: "Yoda",
-    image: "./assets/images/yoda.jpeg",
+    image: "./assets/images/yoda.jpg",
     health: 100,
     attackPower: 8,
     counterAttackPower: 15
@@ -14,7 +14,23 @@ function addCharacterSelector(char) {
     $("#characterSelect").prepend(name, img, health)
 }
 
+function Character(name, image, health, attackPower, counterAttackPower) {
+    this.name = name;
+    this.image = image;
+    this.health = health;
+    this.attackPower = attackPower;
+    this.counterAttackPower = counterAttackPower;
+}
+
+let yoda = new Character("Yoda", "./assets/images/yoda.jpg", 100, 8, 15)
+let sidious = new Character("Darth Sidious", "./assets/images/sidious.jpg", 100, 8, 15)
+let leia = new Character("Leia", "./assets/images/leia.jpg", 100, 8, 15)
+let maul = new Character("Darth Maul", "./assets/images/maul.jpg", 100, 8, 15)
+
 
 // $("#characterSelect").prepend("Some appended text.")
 
-addCharacterSelector(char1)
+addCharacterSelector(yoda)
+addCharacterSelector(sidious)
+addCharacterSelector(leia)
+addCharacterSelector(maul)
