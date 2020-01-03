d05a78dd2695a2abd59fc2a21ea79caceeada201
Cefalu Wave <luke.evan.graham@gmail.com>
2020-01-02 20:36:13 -0800

    added characters to an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,3 @@
-let char1 = {
-  name: "Yoda",
-  image: "./assets/images/yoda.jpg",
-  health: 100,
-  attackPower: 8,
-  counterAttackPower: 15
-};
-
-function addCharacterSelector(char) {
-  console.log("CHAR: ", char);
-  let name = "<p class='center-align'>" + char.name + "</p>";
-  let img = `<img src="` + char.image + `" alt="" class="charImg">`;
-  let health = "<p class='center-align'>" + char.health + "</p>";
-  $("#characterSelect").append(`<div class="col s3"><div class="card charCard">
-  <div class="card-content charCard">` + name + img + health + `</div></div></div>`);
-}
-
 function Character(name, image, health, attackPower, counterAttackPower) {
   this.name = name;
   this.image = image;
@@ -23,20 +6,36 @@ function Character(name, image, health, attackPower, counterAttackPower) {
   this.counterAttackPower = counterAttackPower;
 }
 
-let yoda = new Character("Yoda", "./assets/images/yoda.jpg", 100, 8, 15);
-let sidious = new Character(
-  "Darth Sidious",
-  "./assets/images/sidious.jpg",
-  100,
-  8,
-  15
+characters = [];
+
+characters.push(new Character("Yoda", "./assets/images/yoda.jpg", 100, 8, 15));
+characters.push(
+  new Character("Darth Sidious", "./assets/images/sidious.jpg", 100, 8, 15)
 );
-let leia = new Character("Leia", "./assets/images/leia.jpg", 100, 8, 15);
-let maul = new Character("Darth Maul", "./assets/images/maul.jpg", 100, 8, 15);
+characters.push(new Character("Leia", "./assets/images/leia.jpg", 100, 8, 15));
+characters.push(
+  new Character("Darth Maul", "./assets/images/maul.jpg", 100, 8, 15)
+);
 
-// $("#characterSelect").prepend("Some appended text.")
+function addCharacterSelector(char) {
+  console.log("CHAR: ", char);
 
-addCharacterSelector(yoda);
-addCharacterSelector(sidious);
-addCharacterSelector(leia);
-addCharacterSelector(maul);
+  let index = 0;
+  char.forEach(character => {
+    console.log("INDEX: ", index)
+    let name = "<p class='center-align'>" + character.name + "</p>";
+    let img = `<img src="` + character.image + `" alt="" class="charImg">`;
+    let health = "<p class='center-align'>" + character.health + "</p>";
+    $("#characterSelect").append(
+      `<div class="col s3"><div class="card">
+  <div class="card-content charCard" id="char`+ index +`">` +
+        name +
+        img +
+        health +
+        `</div></div></div>`
+    );
+    index++
+  });
+}
+
+addCharacterSelector(characters);
