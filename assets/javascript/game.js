b88af2937415422da7da9fbcf1db40dd03bd3ac4
Cefalu Wave <luke.evan.graham@gmail.com>
2020-01-02 21:36:01 -0800

    enemies adding to their div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ function Character(name, image, health, attackPower, counterAttackPower) {
 }
 
 characters = [];
+let selectedCharacterIndex;
 
 characters.push(new Character("Yoda", "./assets/images/yoda.jpg", 100, 8, 15));
 characters.push(
@@ -22,19 +23,79 @@ function addCharacterSelector(char) {
 
   let index = 0;
   char.forEach(character => {
-    console.log("INDEX: ", index)
+    console.log("INDEX: ", index);
     let name = "<p class='center-align'>" + character.name + "</p>";
     let img = `<img src="` + character.image + `" alt="" class="charImg">`;
     let health = "<p class='center-align'>" + character.health + "</p>";
     $("#characterSelect").append(
       `<div class="col s3"><div class="card">
-  <div class="card-content charCard" id="char`+ index +`">` +
+  <div class="card-content charCard" id="char` +
+        index +
+        `">` +
         name +
         img +
         health +
         `</div></div></div>`
     );
-    index++
+    index++;
+  });
+
+  listenForCharacterSelection();
+}
+
+function listenForCharacterSelection() {
+  $(".charCard").on("click", function() {
+    if (!selectedCharacterIndex) {
+      selectedCharacterIndex = this.id.substring(4);
+
+      let name =
+        "<p class='center-align'>" +
+        characters[this.id.substring(4)].name +
+        "</p>";
+      let img =
+        `<img src="` +
+        characters[this.id.substring(4)].image +
+        `" alt="" class="charImg">`;
+      let health =
+        "<p class='center-align'>" +
+        characters[this.id.substring(4)].health +
+        "</p>";
+      $("#characterSelect").replaceWith(
+        `<div class="col s4"><div class="card">
+        <div class="card-content charCard" id="char` +
+          this.id.substring(4) +
+          `">` +
+          name +
+          img +
+          health +
+          `</div></div></div>`
+      );
+
+      let index = 0;
+      characters.forEach(char => {
+        if (char.name != characters[selectedCharacterIndex].name ) {
+          console.log("look here: ", char.name, index)
+          let name = "<p class='center-align'>" + char.name + "</p>";
+    let img = `<img src="` + char.image + `" alt="" class="charImg">`;
+    let health = "<p class='center-align'>" + char.health + "</p>";
+    $("#enemies").append(
+      `<div class="col s4"><div class="card">
+  <div class="card-content enemyCard" id="char` +
+        index +
+        `">` +
+        name +
+        img +
+        health +
+        `</div></div></div>`
+    );
+        }
+        index++;
+      });
+      listenForCharacterSelection()
+    }
+    else {
+      console.log("Char selected!")
+    }
   });
 }
 
